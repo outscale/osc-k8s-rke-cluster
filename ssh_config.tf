37cb11cb609df0db233b03b39428f2bd713e5649
Maxime Dufour <maxime.dufour@outscale.com>
2022-09-01 18:41:02 +0000

    Adapt ssh_config for public cloud

--- ssh_config.tf
+++ ssh_config.tf
@@ -3,8 +3,8 @@ resource "local_file" "ssh_config" {
   file_permission = "0660"
   content = format("%s\n\n%s\n\n%s\n",
     format("Host bastion\n     HostName %s\n     User outscale\n     IdentityFile bastion/bastion.pem\n     IdentitiesOnly yes\n     UserKnownHostsFile known_hosts\n     StrictHostKeyChecking accept-new", outscale_public_ip.bastion.public_ip),
-    join("\n\n", [for i in range(var.control_plane_count) : format("Host control-plane-%d\n     HostName 10.0.1.%d\n     ProxyJump bastion\n     User outscale\n     IdentityFile control-planes/control-plane-%s.pem\n     IdentitiesOnly yes\n     UserKnownHostsFile known_hosts\n     StrictHostKeyChecking accept-new", i, 10 + i, i)]),
-    join("\n\n", [for i in range(var.worker_count) : format("Host worker-%d\n     HostName 10.0.1.%d\n     ProxyJump bastion\n     User outscale\n     IdentityFile workers/worker-%s.pem\n     IdentitiesOnly yes\n     UserKnownHostsFile known_hosts\n     StrictHostKeyChecking accept-new", i, 19 + i, i)])
+    join("\n\n", [for i in range(var.control_plane_count) : format("Host control-plane-%d\n     %s     User outscale\n     IdentityFile control-planes/control-plane-%s.pem\n     IdentitiesOnly yes\n     UserKnownHostsFile known_hosts\n     StrictHostKeyChecking accept-new", i, var.public_cloud ? format("HostName %s\n", outscale_public_ip.control-planes[i].public_ip) : format("HostName 10.0.1.%d\n     ProxyJump bastion\n", 10 + i), i)]),
+    join("\n\n", [for i in range(var.worker_count) : format("Host worker-%d\n     %s     User outscale\n     IdentityFile workers/worker-%s.pem\n     IdentitiesOnly yes\n     UserKnownHostsFile known_hosts\n     StrictHostKeyChecking accept-new", i, var.public_cloud ? format("HostName %s\n", outscale_public_ip.workers[i].public_ip) : format("HostName 10.0.1.%d\n     ProxyJump bastion\n", 19 + i), i)])
   )
 }
 
